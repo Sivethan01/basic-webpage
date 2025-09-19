9c5694aabc2adbeb217268c29663eadd2ec6d75d
Sivethan01 <sivethan2006@gmail.com>
2025-09-19 06:04:30 +0530

    updated

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-document.getElementById("clickMe").addEventListener("click", function() {
+//Button Click event in two ways
+
+document.getElementById("b1").addEventListener("click", function() {
   alert("Button clicked!");
 });
+function clicked(){
+  alert("Second Button is Clicked");
+}
